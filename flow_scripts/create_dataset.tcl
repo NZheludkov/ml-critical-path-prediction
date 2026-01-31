554b209e37b946091efb318f8b1218afa0633b54
nvgel <nvgel@cs.niisi.ras.ru>
2026-02-01 01:54:29 +0300

    feat: add create_dataset that get critical nets

--- flow_scripts/create_dataset.tcl
+++ flow_scripts/create_dataset.tcl
@@ -1,12 +1,27 @@
-#rm path.txt
+#rm old files if exists
 if [file exists ./paths.txt] {
 	exec rm ./paths.txt
 }
 
-#write all paths unique endpoints to paths.txt
-foreach sta_path [sta::endpoints] {
-	set path [get_full_name $sta_path]
-	report_checks -to $path -path_delay max -digits 3 -format summary >> ./paths.txt
+if [file exists ./critical_nets.txt] {
+	exec rm ./critical_nets.txt
+}
+
+if [file exists ./critical_paths_summary.txt] {
+	exec rm ./critical_paths_summary.txt
+}
+
+if [file exists ./net_labels.csv] {
+	exec rm ./net_labels.csv
+}
+
+#get unique reg2reg paths
+foreach endpoint [sta::endpoints] {
+	set endpoint [get_full_name $endpoint]
+	set a [find_timing_paths -path_group reg2reg -unique_edges_to_endpoint -unique_paths_to_endpoint -sort_by_slack -to $endpoint]
+	if {$a ne ""} {
+		report_checks -path_group reg2reg -unique_edges_to_endpoint -unique_paths_to_endpoint -to $endpoint -path_delay max -digits 3 -format summary >> ./paths.txt
+	}
 }
 
 #report critical nets
@@ -14,3 +29,4 @@ source $flow_dir/flow_scripts/report_critical_path.tcl
 
 #get critical net, labeled
 source $flow_dir/flow_scripts/get_critical_net.tcl
+
