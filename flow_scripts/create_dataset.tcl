7396f75f5c79275efff83fa7afe4ddaa2b5bd95e
nvgel <nvgel@cs.niisi.ras.ru>
2026-02-05 00:47:24 +0300

    feat: copy data to dataset after flow

--- flow_scripts/create_dataset.tcl
+++ flow_scripts/create_dataset.tcl
@@ -1,32 +1,11 @@
-#rm old files if exists
-if [file exists ./paths.txt] {
-	exec rm ./paths.txt
-}
+#create dataset dir
+set dataset_dir "../../../dataset/${design}"
+exec mkdir -p $dataset_dir
 
-if [file exists ./critical_nets.txt] {
-	exec rm ./critical_nets.txt
-}
+#cp data
+exec cp -r features $dataset_dir
+exec cp -r place_label $dataset_dir
+exec cp -r route_label $dataset_dir
 
-if [file exists ./critical_paths_summary.txt] {
-	exec rm ./critical_paths_summary.txt
-}
 
-if [file exists ./net_labels.csv] {
-	exec rm ./net_labels.csv
-}
-
-#get unique reg2reg paths
-foreach endpoint [sta::endpoints] {
-	set endpoint [get_full_name $endpoint]
-	set a [find_timing_paths -path_group reg2reg -unique_edges_to_endpoint -unique_paths_to_endpoint -sort_by_slack -to $endpoint]
-	if {$a ne ""} {
-		report_checks -path_group reg2reg -unique_edges_to_endpoint -unique_paths_to_endpoint -to $endpoint -path_delay max -digits 3 -format summary >> ./paths.txt
-	}
-}
-
-#report critical nets
-source $flow_dir/flow_scripts/report_critical_path.tcl
-
-#get critical net, labeled
-source $flow_dir/flow_scripts/get_critical_net.tcl
 
